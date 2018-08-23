ce9b800438ad8161883fe722ac15ea8c95cded13
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-08-23 11:25:38 -0700

    Bare bones express app done

---
+++ index.js
@@ -0,0 +1,22 @@
+// require needed modules
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+
+// declare app variable
+var app = express();
+
+// set amd use statements
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// define routes
+app.get('/', function(req, res){
+  res.send('hi from home route');
+});
+
+// listen on port 3000
+app.listen(3000);
